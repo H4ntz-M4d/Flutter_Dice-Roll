6369cc3723ddc4406da2d511ba21b67fe2bbc447
Rotas0Square <ardhiussalam.06@gmail.com>
2024-03-09 23:29:27 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project_1/navbar.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 117, 2, 216),
+          Color.fromARGB(255, 187, 0, 200),
+        ),
+      ),
+    ),
+  );
+}
